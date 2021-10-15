d8fb472e8539aa7dbad770016e00eed6bb4dff64
Paulo Jefferson Mendes Oliveira <dark_4862@hotmail.com>
2021-10-14 22:46:07 -0300

    Primeira parte criada

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente("Paulo Jefferson Mendes Oliveira", "000.000.000-00", "Desenvolvedor");
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = cliente;
+            conta.agencia = 01;
+            conta.conta = 0001;
+
+            Console.WriteLine("Conta Corrente Criada");
+            Console.WriteLine("Titular: {0}", conta.titular.nome);
+            Console.WriteLine("Agência: {0}", conta.agencia);
+            Console.WriteLine("Conta: {0}", conta.conta);
         }
     }
 }
